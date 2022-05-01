a5c4cfa370266633053100d5edd37473aa02fa61
Rajarshi Samaddar <itsrajarshi@gmail.com>
2022-05-01 10:51:45 +0530

    update

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f^ftrhc=kekf@y=sl)0a4lt&yyqv4brw)9u74%n)=m=t!=vsx3'
+SECRET_KEY = 'weiojufbw e0iweqpoij23090940932409nweijfn0934u(**(&*()*U*@kjdsfouij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
     'work',
     'about',
     # 'home.apps.HomeConfig'
-    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -118,21 +116,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
-]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-
-STATIC_ROOT = BASE_DIR / 'static'
-
-SASS_PROCESSOR_ROOT = STATIC_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
