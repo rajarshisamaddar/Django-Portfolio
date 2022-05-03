6573ba65a04d0c2341fbde596f7dd3ff49374bd7
Rajarshi Samaddar <itsrajarshi@gmail.com>
2022-05-03 12:42:52 +0530

    03-05-2022 update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -109,7 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = "django-portfolio/www/public"
 
 MEDIA_URL = 'img/'
 
@@ -121,3 +123,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    
