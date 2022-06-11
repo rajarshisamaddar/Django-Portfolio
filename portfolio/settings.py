8ee382d3710ab8be2ee14132a93b94682c0630b4
Rajarshi Samaddar <itsrajarshi@gmail.com>
2022-06-11 14:48:42 +0530

    project blog update 🚀❤

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -72,13 +72,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'database',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
 }
 
 
